561b1c84c2c4a1e11e4107fc4836bcdfff38b269
Matt Pike <mattp91@gmail.com>
2023-09-04 16:45:36 -0400

    Added more routes / article functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles # creates routes method
+
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 end
