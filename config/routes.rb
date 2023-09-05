b88d90ccb85c0e395ee793a81f1dce58950b9662
Matt Pike <mattp91@gmail.com>
2023-09-05 15:31:17 -0400

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles # creates a resource for articles, with preset routing methods
-    resources :comments # creates a nested resource for comments within the articles resource
-    
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do
+    resources :comments
+  end
 end
